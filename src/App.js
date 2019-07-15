c771dc90a9444d2cca9942e90ea950c4bf9719b7
sanscomp <sanscomp@anslems-iMac.lan>
2019-07-15 20:36:55 +0100

    Add header & todo list components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
-import React from 'react';
+import React, { useState }from 'react';
+import Header from './components/Header';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Todo list</h1>
-    </div>
-  );
+function Todo({ todo, index, completeTodo, removeTodo }) {
+  return <div className='todo'>{todo.text}</div>   
 }
 
+function App() {
+
+  const [todos, setTodo] = useState([
+    {
+      text: 'Use react hooks',
+      isCompleted: false
+    },
+    {
+      text: 'Write some code',
+      isCompleted: false
+    },
+    {
+      text: 'Build this app',
+      isCompleted: false
+    }
+
+  ]);
+
+  return (<div className='app'>
+    <Header/>
+    <div className='todo-list'>
+      {todos.map((todo, index) => (
+        <Todo key={index}
+          index={index}
+          todo={todo}
+        />
+      ))}
+    </div>
+  </div>)
+}
+
+
 export default App;
+
